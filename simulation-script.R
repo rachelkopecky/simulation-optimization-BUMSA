2a3a7eada0cb4564b12c5d431d10b39450a8f01e
Derek Kopecky <dkopecky@appriss.com>
2018-02-28 11:55:27 -0500

    Added the "yellow card" attribute, that the deck will reset ones a certain number of cards are left

--- simulation-script.R
+++ simulation-script.R
@@ -44,51 +44,70 @@ x3win <- 0
 #x3wal <- 100
 
 ## Shuffle Deck ##
-deck <- deck[sample(nrow(deck)),]
+deck1 <- deck[sample(nrow(deck)),]
+
+## Set Value to Number of games to simulate ##
+n <- 100
+
+#### Rule where Players stay on 16 or higher ####
+for (i in 1:n) {
+
+  if(nrow(deck1) < 65){
+    deck1 <- deck[sample(nrow(deck)),]
+  } else{
+    deck1 <- deck1
+  }
+
+# Dealer #
+y <- NULL 
+# 3 Players #
+x1 <- NULL
+x2 <- NULL
+x3 <- NULL
 
 ## Simluate the Initial Deal ##
-for (i in 1:nrow(deck)) {
+for (i in 1:nrow(deck1)) {
         ## Assign Each Player and Dealer First Card
-        x1 <- deck[sample(nrow(deck), 1), ]
-        deck <- deck[-as.integer(rownames(x1[1,])),]
-        x2 <- deck[sample(nrow(deck), 1), ]
-        deck <- deck[-as.integer(rownames(x2[1,])),]
-        x3 <- deck[sample(nrow(deck), 1), ]
-        deck <- deck[-as.integer(rownames(x3[1,])),]
-        y <- deck[sample(nrow(deck), 1), ]
-        deck <- deck[-as.integer(rownames(y[1,])),]
+        x1 <- deck1[sample(nrow(deck1), 1), ]
+        deck1 <- deck1[-as.integer(rownames(x1[1,])),]
+        x2 <- deck1[sample(nrow(deck1), 1), ]
+        deck1 <- deck1[-as.integer(rownames(x2[1,])),]
+        x3 <- deck1[sample(nrow(deck1), 1), ]
+        deck1 <- deck1[-as.integer(rownames(x3[1,])),]
+        y <- deck1[sample(nrow(deck1), 1), ]
+        deck1 <- deck1[-as.integer(rownames(y[1,])),]
         
         ## Assign Each Player and Dealer Second Card
-        x1 <- rbind(x1, deck[sample(nrow(deck), 1), ])
-        deck <- deck[-as.integer(rownames(x1[2,])),]
-        x2 <- rbind(x2, deck[sample(nrow(deck), 1), ])
-        deck <- deck[-as.integer(rownames(x2[2,])),]
-        x3 <- rbind(x3, deck[sample(nrow(deck), 1), ])
-        deck <- deck[-as.integer(rownames(x3[2,])),]
-        y <- rbind(y, deck[sample(nrow(deck), 1), ])
-        deck <- deck[-as.integer(rownames(y[2,])),]
+        x1 <- rbind(x1, deck1[sample(nrow(deck1), 1), ])
+        deck1 <- deck1[-as.integer(rownames(x1[2,])),]
+        x2 <- rbind(x2, deck[sample(nrow(deck1), 1), ])
+        deck1 <- deck1[-as.integer(rownames(x2[2,])),]
+        x3 <- rbind(x3, deck[sample(nrow(deck1), 1), ])
+        deck1 <- deck1[-as.integer(rownames(x3[2,])),]
+        y <- rbind(y, deck[sample(nrow(deck1), 1), ])
+        deck1 <- deck1[-as.integer(rownames(y[2,])),]
         
         break
 }
 
 ## Simluate Hitting or Staying for Round ##
 # With rule that if player has less then 16, then hit, else stay #
-for (i in 1:nrow(deck)) {
+for (i in 1:nrow(deck1)) {
         if(sum(y[,3]) == 21){
                 ywin <- ywin + 1
                 break
         } else if(sum(x1[,3]) < 16) {
-                x1 <- rbind(x1, deck[sample(nrow(deck), 1), ])
-                deck <- deck[-as.integer(rownames(x1)),]
+                x1 <- rbind(x1, deck[sample(nrow(deck1), 1), ])
+                deck1 <- deck1[-as.integer(rownames(x1)),]
         } else if(sum(x2[,3]) < 16){
-                x2 <- rbind(x2, deck[sample(nrow(deck), 1), ])
-                deck <- deck[-as.integer(rownames(x2)),]
+                x2 <- rbind(x2, deck[sample(nrow(deck1), 1), ])
+                deck1 <- deck1[-as.integer(rownames(x2)),]
         } else if(sum(x3[,3]) < 16){
-                x3 <- rbind(x3, deck[sample(nrow(deck), 1), ])
-                deck <- deck[-as.integer(rownames(x3)),]
+                x3 <- rbind(x3, deck[sample(nrow(deck1), 1), ])
+                deck1 <- deck1[-as.integer(rownames(x3)),]
         } else if(sum(y[,3]) < 16){
-                y <- rbind(y, deck[sample(nrow(deck), 1), ])
-                deck <- deck[-as.integer(rownames(y)),]
+                y <- rbind(y, deck[sample(nrow(deck1), 1), ])
+                deck1 <- deck1[-as.integer(rownames(y)),]
         } else {
                 break
         }
@@ -104,3 +123,9 @@ ysum <- ifelse(sum(y[,3]) > 21, 0, sum(y[,3]))
 x1win <- ifelse(x1sum > ysum, (x1win + 1), x1win)
 x2win <- ifelse(x2sum > ysum, (x2win + 1), x2win)
 x3win <- ifelse(x3sum > ysum, (x3win + 1), x3win)
+
+}
+
+playerwins <- rbind(x1win, x2win, x3win, ywin)
+
+
